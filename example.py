9d8b76e5b083e5d59f2c42f7a844b0558bd52698
Lucy Follansbee <lfollansbee@gmail.com>
2019-09-23 11:55:34 -0600

    increment and decrement one score

--- example.py
+++ example.py
@@ -3,10 +3,21 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+score_1 = 0
+score_2 = 0
 
 while True:
-    input_state = GPIO.input(18)
-    if input_state == False:
-        print('Button Pressed')
+    input_state_4 = GPIO.input(4)
+    input_state_5 = GPIO.input(5)
+    if input_state_4 == False:
+        print('Score +1')
+        score_1 += 1
+        time.sleep(0.2)
+
+    if input_state_5 == False:
+        print('Score -1')
+        score_1 -= 1
         time.sleep(0.2)
