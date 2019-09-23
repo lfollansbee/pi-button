058762839a99bb1135dd5bbc1734ea2eca4044c4
Lucy Follansbee <lfollansbee@gmail.com>
2019-09-23 15:54:13 -0600

    Update example.py

--- example.py
+++ example.py
@@ -10,6 +10,8 @@ GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(20, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 score_1 = 0
 score_2 = 0
 
@@ -19,42 +21,39 @@ while True:
 
     input_state_20 = GPIO.input(20)
     input_state_21 = GPIO.input(21)
+    
+    input_state_26 = GPIO.input(26)
 
-    #Player 1
+    # Player 1
     if input_state_4 == False:
-    #    print('Score1 +1')
         score_1 += 1
-#         print(score_1)
         time.sleep(.4)
 
     if input_state_5 == False:
-    #    print('Score1 -1')
         score_1 -= 1
         if score_1 <= 0:
             score_1 = 0
-#         print(score_1)
         time.sleep(0.4)
 
-    #Player 2
+    # Player 2
     if input_state_20 == False:
-#         print('Score2 +1')
         score_2 += 1
-#         print(score_2)
         time.sleep(.4)
 
     if input_state_21 == False:
-#         print('Score2 -1')
         score_2 -= 1
         if score_2 <= 0:
             score_2 = 0
-#         print(score_2)
         time.sleep(0.4)
-    
-    if input_state_4 == False or input_state_5 == False or input_state_20 == False or input_state_21 == False:
-        print('PLAYER1 SCORE')
-        print(score_1)
-        print('PLAYER2 SCORE')
-        print(score_2)
         
-# RESET
+    # RESET
+    if input_state_26 == False:
+        score_1 == 0
+        score_2 == 0
+        print("BIG BLUE BUTTON clicked, Reset score")
+        time.sleep(.4)
+    
+    if input_state_4 == False or input_state_5 == False or input_state_20 == False or input_state_21 == False or input_state_26 == False:
+        print(score_1, score_2)
+        
 
