727c6b11b19f10dfa996d88e1da049637f7b9191
Lucy Follansbee <lfollansbee@gmail.com>
2019-09-24 10:36:32 -0600

    update python object to send via MQTT

--- example.py
+++ example.py
@@ -17,10 +17,11 @@ GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 MQTT_SERVER = "54.213.60.205"
 MQTT_PATH = "mychanel"
 
-score = {
-    "score_1": 0,
-    "score_2": 0
-}
+class Score:
+  score_1 = 0
+  score_2 = 0
+
+score = Score()
 
 while True:
     input_state_4 = GPIO.input(4)
@@ -34,37 +35,35 @@ while True:
     # Player 1
     if input_state_4 == False:
         score.score_1 += 1
-        publish.single(MQTT_PATH, score, hostname=MQTT_SERVER)
+        publish.single(MQTT_PATH, json.dumps(score.__dict__), hostname=MQTT_SERVER)
         time.sleep(.4)
 
     if input_state_5 == False:
         score.score_1 -= 1
         if score.score_1 <= 0:
             score.score_1 = 0
-        publish.single(MQTT_PATH, score, hostname=MQTT_SERVER)
+        publish.single(MQTT_PATH, json.dumps(score.__dict__), hostname=MQTT_SERVER)
         time.sleep(0.4)
 
     # Player 2
     if input_state_20 == False:
         score.score_2 += 1
-        publish.single(MQTT_PATH, score, hostname=MQTT_SERVER)
+        publish.single(MQTT_PATH, json.dumps(score.__dict__), hostname=MQTT_SERVER)
         time.sleep(.4)
 
     if input_state_21 == False:
         score.score_2 -= 1
         if score_2 <= 0:
             score.score_2 = 0
-        publish.single(MQTT_PATH, score, hostname=MQTT_SERVER)
+        publish.single(MQTT_PATH, json.dumps(score.__dict__), hostname=MQTT_SERVER)
         time.sleep(0.4)
         
     # RESET
     if input_state_26 == False:
         score.score_1 = 0
         score.score_2 = 0
-        publish.single(MQTT_PATH, score, hostname=MQTT_SERVER)
+        publish.single(MQTT_PATH, json.dumps(score.__dict__), hostname=MQTT_SERVER)
         time.sleep(.4)
     
     if input_state_4 == False or input_state_5 == False or input_state_20 == False or input_state_21 == False or input_state_26 == False:
         print(score.score_1, score.score_2)
-        
-
